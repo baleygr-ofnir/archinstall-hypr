8772864daaba833df51dabcca66eb3c77fdc2aee
baleygr-ofnir <jorma.lindh@mimirsbrunnr.cloud>
2025-07-05 10:19:21 +0200

    realtime priv

--- lib/install.sh
+++ lib/install.sh
@@ -41,7 +41,7 @@ sed -i -e '/^#\?\[extra\]/s/^#//' \
     -e '/^#\?\[multilib\]/s/^#//' \
     -e '/^\[multilib\]/,+1{/^#\?Include.*mirrorlist/s/^#//}' \
     /etc/pacman.conf
-pacman -Sy --noconfirm git sudo
+pacman -Sy --noconfirm git sudo realtime-privileges
 
 # User configuration
 echo "Creating user USERNAME_PLACEHOLDER..."
